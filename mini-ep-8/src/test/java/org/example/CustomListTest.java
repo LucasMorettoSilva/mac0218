282909955f5403d3a0a1a3c5fd014be1149ccc7a
Lucas Moretto da Silva <lucas.da.silva1@dell.com>
2022-06-09 08:43:04 -0300

    adicionando outro cenário de teste para o método isEmpty()

--- mini-ep-8/src/test/java/org/example/CustomListTest.java
+++ mini-ep-8/src/test/java/org/example/CustomListTest.java
@@ -2,6 +2,9 @@ package org.example;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CustomListTest {
@@ -13,5 +16,15 @@ public class CustomListTest {
         // agora o teste deve passar, visto
         // que existe lógica real no método
         assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void givenNonEmptyList_whenIsEmpty_thenFalseIsReturned() {
+        CustomList<String> list = new CustomList<>();
+        list.add("my string");
+
+        // testando outro cenário
+        // também deve passar
+        assertFalse(list.isEmpty());
     }
 }
